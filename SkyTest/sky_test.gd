0666f0ef9c7db12b252a2a2d88111f2b870ea6c7
Kris <fernandoprofitt@gmail.com>
2025-05-14 12:41:56 -0700

    please work

--- SkyTest/sky_test.gd
+++ SkyTest/sky_test.gd
@@ -3,12 +3,22 @@ extends Node3D
 
 @export_tool_button("Picture") 
 var button = _picture
+var exposing = false
+@export
+var Max_Exposure : int = 10
+var frames_exposed : int = 0
+
+@export
+var viewport : Viewport = EditorInterface.get_editor_viewport_3d(0)
+var images : Array[Image] = []
 
 var timelist : PackedFloat32Array =[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
 									1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
 									1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
 									1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
 var time_acc := 0.1
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timelist.append_array(timelist) #80
@@ -21,14 +31,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if time_acc > 1.0:
+	if time_acc > 10000.0:
 		time_acc = 0.10
 	time_acc += delta
 	timelist.remove_at(0)
 	timelist.append(time_acc)
 	$WorldEnvironment.environment.sky.sky_material.set("shader_parameter/seeds", timelist)
+	
+	if exposing:
+		frames_exposed = frames_exposed + 1
+		images.append(viewport.get_texture().get_image())
+		print("Image Captured:", frames_exposed)
+	
+	if frames_exposed == Max_Exposure:
+		var i : Image = Image.create_empty(images[0].get_width(),images[0].get_height(), false, images[0].get_format())
+		for x in range(images[0].get_width()):
+			for y in range(images[0].get_height()):
+				var px = Color()
+				for j in Max_Exposure:
+					px += images[j].get_pixel(x, y)
+				px = px * (1.0 / (Max_Exposure))
+				i.set_pixel(x, y, px)
+		var filename = "user://Screenshot " + str(Time.get_ticks_msec()) + ".png"
+		i.save_png(filename)
+		exposing = false
+		frames_exposed = 0
+		images.clear()
+		print("DONE")
 
 func _picture():
-	var capture = EditorInterface.get_editor_viewport_3d(0).get_texture().get_image()
-	var filename = "user://Screenshot-{0}.png"
-	capture.save_png(filename)
+	exposing = true
